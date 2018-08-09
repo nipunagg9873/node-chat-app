77cc08d794c459acc7730b24d9b11a2976f4e34b
nipunagg9873 <nipunagg9873@gmail.com>
2018-08-09 17:25:04 +0530

    add form for messages and show incomming messages

--- server/server.js
+++ server/server.js
@@ -27,8 +27,9 @@ io.on('connection',(socket)=>{
 
   socket.broadcast.emit('newMessage',generateMessage('Admin','new user joined'));
 
-  socket.on('createMessage',(message)=>{
+  socket.on('createMessage',(message,callback)=>{
     io.emit('newMessage',generateMessage(message.from,message.text));
+    callback('data recieved');
   });
 
   socket.on('disconnect',()=>{
